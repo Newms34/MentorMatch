f1f2519bedebc876834ab6c23578a5319f91f732
Newms34 <newms3450@gmail.com>
2019-10-26 18:12:27 -0400

    removed HTTPS since apparently that does not work

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'),
     app = express(),
-    http = process.env.NODE_ENV=='production'? require('https'):require('http'),
+    http = require('http'),
     server = http.Server(app),
     io = require('socket.io')(server),
     path = require('path'),
