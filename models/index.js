eecadffb1e3dd7d41f98d9342a61a4d737394788
Newms34 <newms3450@gmail.com>
2019-10-27 17:26:10 -0400

    fixed mongoose vuln

--- models/index.js
+++ models/index.js
@@ -2,15 +2,13 @@ const mongoose = require('mongoose');
 require('./users/');
 require('./lessonReq/');
 require('./topics/');
-console.log('Node Environment:', process.env.NODE_ENV);
-if (!process.env.NODE_ENV || process.env.NODE_ENV != 'production') {
-    //just some quick env check. If we're developing locally, go ahead and use our local db. Otherwise, use the mlab db.
-    mongoose.connect('mongodb://localhost:27017/codementormatch',{useNewUrlParser:true});
-} else {
-    mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true});
-}
+console.log('Node Environment:', process.env.NODE_ENV||'(unknown)');
+mongoose.connect(process.env.NODE_ENV && process.env.NODE_ENV=='production'?process.env.MONGODB_URI:'mongodb://localhost:27017/codementormatch', {
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(e) {
+db.once('open', function (e) {
     console.log('Database connected!');
 });
